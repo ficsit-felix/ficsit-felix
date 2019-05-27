17a17e7ffb6c76887d77b2059f9a11cb63436c81
bitowl <git@bitowl.de>
2019-05-27 23:31:37 +0200

    Clean up compass svg as it already looks cool without text
    Closes #8

--- app/src/components/Compass.vue
+++ app/src/components/Compass.vue
@@ -16,43 +16,6 @@
    id="svg8"
    inkscape:version="0.92.4 5da689c313, 2019-01-14"
    sodipodi:docname="compass.svg">
-  <defs
-     id="defs2" />
-  <sodipodi:namedview
-     id="base"
-     pagecolor="#ffffff"
-     bordercolor="#666666"
-     borderopacity="1.0"
-     inkscape:pageopacity="0.0"
-     inkscape:pageshadow="2"
-     inkscape:zoom="2.8"
-     inkscape:cx="75.931616"
-     inkscape:cy="43.728434"
-     inkscape:document-units="mm"
-     inkscape:current-layer="layer1"
-     showgrid="false"
-     inkscape:window-width="1920"
-     inkscape:window-height="1062"
-     inkscape:window-x="1920"
-     inkscape:window-y="18"
-     inkscape:window-maximized="0"
-     fit-margin-top="2"
-     fit-margin-left="2"
-     fit-margin-right="2"
-     fit-margin-bottom="2"
-     inkscape:pagecheckerboard="true" />
-  <metadata
-     id="metadata5">
-    <rdf:RDF>
-      <cc:Work
-         rdf:about="">
-        <dc:format>image/svg+xml</dc:format>
-        <dc:type
-           rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
-        <dc:title></dc:title>
-      </cc:Work>
-    </rdf:RDF>
-  </metadata>
   <g
      inkscape:label="Layer 1"
      inkscape:groupmode="layer"
@@ -62,50 +25,6 @@
        id="compass"
        inkscape:transform-center-x="1.5119048"
        inkscape:transform-center-y="0.18898972">
-      <text
-         id="textN"
-         y="24.234615"
-         x="49.381027"
-         style="font-style:normal;font-weight:normal;font-size:4.93888903px;line-height:1.25;font-family:sans-serif;letter-spacing:0px;word-spacing:0px;fill:#ffffff;fill-opacity:1;stroke:none;stroke-width:0.26458332"
-         xml:space="preserve"><tspan
-           style="font-size:4.93888903px;fill:#ffffff;fill-opacity:1;stroke-width:0.26458332"
-           y="24.234615"
-           x="49.381027"
-           id="tspan4562"
-           sodipodi:role="line">N</tspan></text>
-      <text
-         xml:space="preserve"
-         style="font-style:normal;font-weight:normal;font-size:4.93888903px;line-height:1.25;font-family:sans-serif;letter-spacing:0px;word-spacing:0px;fill:#ffffff;fill-opacity:1;stroke:none;stroke-width:0.26458332"
-         x="29.951014"
-         y="42.513271"
-         id="textW"><tspan
-           sodipodi:role="line"
-           id="tspan4566"
-           x="29.951014"
-           y="42.513271"
-           style="font-size:4.93888903px;fill:#ffffff;fill-opacity:1;stroke-width:0.26458332">W</tspan></text>
-      <text
-         id="textS"
-         y="60.721992"
-         x="49.635448"
-         style="font-style:normal;font-weight:normal;font-size:4.93888903px;line-height:1.25;font-family:sans-serif;letter-spacing:0px;word-spacing:0px;fill:#ffffff;fill-opacity:1;stroke:none;stroke-width:0.26458332"
-         xml:space="preserve"><tspan
-           style="font-size:4.93888903px;fill:#ffffff;fill-opacity:1;stroke-width:0.26458332"
-           y="60.721992"
-           x="49.635448"
-           id="tspan4570"
-           sodipodi:role="line">S</tspan></text>
-      <text
-         xml:space="preserve"
-         style="font-style:normal;font-weight:normal;font-size:4.93888903px;line-height:1.25;font-family:sans-serif;letter-spacing:0px;word-spacing:0px;fill:#ffffff;fill-opacity:1;stroke:none;stroke-width:0.26458332"
-         x="66.702103"
-         y="42.513271"
-         id="textE"><tspan
-           sodipodi:role="line"
-           id="tspan4574"
-           x="66.702103"
-           y="42.513271"
-           style="font-size:4.93888903px;fill:#ffffff;fill-opacity:1;stroke-width:0.26458332">E</tspan></text>
       <g
          id="inner" v-bind:style="{ transform: 'rotateX(' + rotateX + 'rad) rotateZ('+rotateZ+'rad)'}">
         <circle
@@ -151,10 +70,6 @@
 #inner {
     /* very wired origin point due to SVG shenanigans */
     transform-origin: 111% 92%;
-}
-/* text should rotate with as well, requires some more complicated calculations, so disabled for now. might even look better without textgit  */
-#textW, #textN, #textE, #textS {
-    display: none;
 }
 </style>
 
