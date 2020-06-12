6dc069ff0dcb4d6a547eecc19f0bb924aab76112
bitowl <git@bitowl.de>
2020-06-12 18:32:49 +0200

    Increase map resolutions

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   },
 
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'prettier/prettier': [
       process.env.NODE_ENV === 'production' ? 'error' : 'warn',
