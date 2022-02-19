8f3a7aa10989f3387ff8c2c56df7bbcc3390b5ee
bitowl <git@bitowl.de>
2022-02-19 12:05:00 +0100

    Remove error reporting

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,8 +2,6 @@ import '@lib/cmdHelper';
 import { CHANGE_LOCALE } from '@lib/constants';
 import { EventBus } from '@lib/event-bus';
 import { isElectron } from '@lib/isElectron';
-import * as Sentry from '@sentry/browser';
-import * as Integrations from '@sentry/integrations';
 import Vue from 'vue';
 import vgl from 'vue-golden-layout';
 import Router from 'vue-router';
@@ -11,18 +9,6 @@ import './assets/golden-layout-dark.css';
 import { i18n } from './plugins/i18n';
 import vuetify from './plugins/vuetify';
 import store from './store';
-
-if (process.env.NODE_ENV !== 'development') {
-  Sentry.init({
-    dsn: 'https://4bee35ee7cba4ba194c9e1a575948656@sentry.io/1416938',
-    release: process.env.PACKAGE_VERSION,
-    integrations: [
-      new Integrations.Vue({
-        attachProps: true
-      })
-    ]
-  });
-}
 
 // vue-shortkey
 Vue.use(require('vue-shortkey'));
